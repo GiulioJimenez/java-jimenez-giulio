613ed7ca052e75f07f3c4d42fae64fd3ccebfc4f
GiulioJimenez <giulioabel10@gmail.com>
2021-02-26 10:19:22 +0100

    esempio di polimorfismo

---
+++ Polimorfismo-esempio/src/Padre.java
@@ -0,0 +1,10 @@
+public class Padre {
+        public void metodo1() {
+                System.out.println("Padre.metodo1()");
+        }
+
+        public void metodo2() {
+                System.out.println("Padre.metodo2()");
+        }
+
+}
