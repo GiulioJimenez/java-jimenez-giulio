47b349c1bb13db08382740196db7e8bbcaa84063
GiulioJimenez <giulioabel10@gmail.com>
2021-02-26 12:14:16 +0100

    strutture dati - Compiti

--- Strutture-dati/src/List.java
+++ Strutture-dati/src/List.java
@@ -2,4 +2,24 @@ public abstract class List {
 
     public abstract List addLast(int x);
     public abstract void addLast2(int x);
+    public abstract List add(int x, int i);
+
+    // rimuove tutte le occorenze di x
+    public abstract List remove(int x);
+
+    // rimuove l'elemento in posizione i
+    public abstract List removeAtIndex(int i);
+
+    // raddoppia tutti gli elementi della lista
+    public abstract List doppio();
+
+    // true se tutti gli elementi sono pari
+    public abstract boolean pari();
+
+    // true se la lista è ordinata
+    public abstract boolean ordinata(int next);
+
+    // per i forti: true se è ordinata crescente o decrescente, false altrimenti
+    // public abstract boolean ordinataCrescenteDecrescente(....)
+    // suggerimento: io definerei una enumerazione
 }
