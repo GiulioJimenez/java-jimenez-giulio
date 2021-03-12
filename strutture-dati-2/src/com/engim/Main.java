025fea666322b51b850adc79265b5875cd9ec11e
GiulioJimenez <giulioabel10@gmail.com>
2021-03-12 11:09:47 +0100

    strutture dati-2 + calcolatrice

---
+++ strutture-dati-2/src/com/engim/Main.java
@@ -0,0 +1,22 @@
+package com.engim;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> lista = new ArrayList<>();
+
+        lista.add("Ciao");
+        lista.add("Ciao2");
+
+        for(String s: lista) {
+            System.out.println(s);
+        }
+        lista.remove("Ciao");
+
+        for(String s: lista) {
+            System.out.println(s);
+        }
+    }
+}
